172e6224ec21a571ef74159a91de242422ed6802
DESKTOP-G7Q9FHC\manuelrc <manuelrc48@educastur.es>
2024-10-18 12:25:18 +0200

    prueba

---
+++ prueba.php
@@ -0,0 +1,12 @@
+<?php
+
+$x = 10;
+$j = 5;
+$t = 8;
+
+if((($x<7) and ($j>3)) or ((4x<7) and ($t>4)) or (($x<7) or ($t<$j)))
+{
+    echo "para los valores x=$x, j=$j, t=$t  la condicion es VERDADERA"
+    else
+    echo "para los valores x=$x, j=$j, t=$t  la condicion es FALSA"
+}
